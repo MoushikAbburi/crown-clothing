8cf81ed51de0680cf91e13dfeceb3dc6ca5d548c
MoushikAbburi <moushikabburi1998@gmail.com>
2021-02-24 13:04:52 -0500

    Added collection component and did some routing

--- src/Pages/Shop/shop.components.jsx
+++ src/Pages/Shop/shop.components.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import CollectionOverview from "../../Components/collection-overview/collection-overview.components";
+import { Route } from "react-router-dom";
+import CollectionPage from "../../Components/collection/collection.components";
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
+  console.log({ match });
   return (
     <div className="shop-page">
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,16 +1,16 @@
 const INITIAL_STATE = {
   sections: [
     {
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-      id: 1,
-      linkUrl: "shop/hats",
-    },
-    {
       title: "jackets",
       imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
       id: 2,
       linkUrl: "shop/jackets",
+    },
+    {
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      id: 1,
+      linkUrl: "shop/hats",
     },
     {
       title: "sneakers",
