e76019a166ead354c9adabef14a4c2af46e34cb5
MoushikAbburi <moushikabburi1998@gmail.com>
2021-02-24 16:56:21 -0500

    Changed the data in array to object

--- src/Pages/Shop/shop.components.jsx
+++ src/Pages/Shop/shop.components.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CollectionOverview from "../../Components/collection-overview/collection-overview.components";
 import { Route } from "react-router-dom";
-import CollectionPage from "../../Components/collection/collection.components";
+import CollectionPage from "../collection/collection.components";
 
 const ShopPage = ({ match }) => {
   console.log({ match });
